cbb75879d5f2e94abd5a2dec3a17d446396774a9
Halthomas65 <58268152+Halthomas65@users.noreply.github.com>
2025-05-12 18:42:33 +0700

    books page
    missing publishdate, price
    not properly aligned

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,12 +1,16 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
 import { BookService, BookDto } from '@proxy/books';
+import { LocalizationModule } from '@abp/ng.core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
+  standalone: true,
   providers: [ListService],
+  imports: [LocalizationModule, NgxDatatableModule], // Import LocalizationModule and NgxDatatableModule here
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
@@ -18,6 +22,7 @@ export class BookComponent implements OnInit {
 
     this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
       this.book = response;
+      console.log(this.book.items); // Debug: Check if data is being populated
     });
   }
 }
