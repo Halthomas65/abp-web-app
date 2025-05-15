f094aacf318e21c93545a67a9be578d2880938fd
Halthomas65 <58268152+Halthomas65@users.noreply.github.com>
2025-05-15 18:03:14 +0700

    authors: user interface

--- angular/src/app/author/author.component.ts
+++ angular/src/app/author/author.component.ts
@@ -1,11 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { AuthorService, AuthorDto } from '@proxy/authors';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '@abp/ng.core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { NgbDatepickerModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-author',
-  standalone: false,
   templateUrl: './author.component.html',
-  styleUrl: './author.component.scss'
+  styleUrls: ['./author.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreModule,
+    NgxDatatableModule,
+    ThemeSharedModule,
+    NgbDatepickerModule,
+    NgbDropdownModule
+  ],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
 })
-export class AuthorComponent {
+export class AuthorComponent implements OnInit {
+  author = { items: [], totalCount: 0 } as PagedResultDto<AuthorDto>;
 
+  isModalOpen = false;
+
+  form: FormGroup;
+
+  selectedAuthor = {} as AuthorDto;
+
+  constructor(
+    public readonly list: ListService,
+    private authorService: AuthorService,
+    private fb: FormBuilder,
+    private confirmation: ConfirmationService
+  ) { }
+
+  ngOnInit(): void {
+    const authorStreamCreator = (query) => this.authorService.getList(query);
+
+    this.list.hookToQuery(authorStreamCreator).subscribe((response) => {
+      this.author = response;
+    });
+  }
+
+  createAuthor() {
+    this.selectedAuthor = {} as AuthorDto;
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  editAuthor(id: string) {
+    this.authorService.get(id).subscribe((author) => {
+      this.selectedAuthor = author;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: [this.selectedAuthor.name || '', Validators.required],
+      birthDate: [
+        this.selectedAuthor.birthDate ? new Date(this.selectedAuthor.birthDate) : null,
+        Validators.required,
+      ],
+      shortBio: [
+        this.selectedAuthor.shortBio ? this.selectedAuthor.shortBio : null,
+        Validators.required,
+      ],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    if (this.selectedAuthor.id) {
+      this.authorService
+        .update(this.selectedAuthor.id, this.form.value)
+        .subscribe(() => {
+          this.isModalOpen = false;
+          this.form.reset();
+          this.list.get();
+        });
+    } else {
+      this.authorService.create(this.form.value).subscribe(() => {
+        this.isModalOpen = false;
+        this.form.reset();
+        this.list.get();
+      });
+    }
+  }
+
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure')
+      .subscribe((status) => {
+        if (status === Confirmation.Status.confirm) {
+          this.authorService.delete(id).subscribe(() => this.list.get());
+        }
+      });
+  }
 }
--- angular/src/app/author/author.module.ts
+++ angular/src/app/author/author.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { AuthorRoutingModule } from './author-routing.module';
 import { AuthorComponent } from './author.component';
-
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CoreModule } from '@abp/ng.core';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
 
 @NgModule({
-  declarations: [
-    AuthorComponent
-  ],
   imports: [
-    CommonModule,
-    AuthorRoutingModule
-  ]
+    SharedModule,
+    AuthorRoutingModule,
+    NgbDatepickerModule,
+    NgxDatatableModule,
+    CoreModule,
+    ThemeSharedModule,
+    AuthorComponent,
+  ],
 })
 export class AuthorModule { }
