09dbf313b99c57c5d766208413b6f002fabecf83
Halthomas65 <58268152+Halthomas65@users.noreply.github.com>
2025-05-15 14:05:47 +0700

    Authors: Domain Layer

---
+++ src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors;
+
+public class Author : FullAuditedAggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+    public DateTime BirthDate { get; set; }
+    public string shortBio { get; set; }
+
+    private Author()
+    {
+        // This constructor is for deserialization / ORM purpose
+    }
+
+    internal Author(Guid id, string name, DateTime birthDate, string? shortBio = null)
+        : base(id)
+    {
+        SetName(name);
+        BirthDate = birthDate;
+        this.shortBio = shortBio;
+    }
+
+    internal Author ChangeName(string name)
+    {
+        SetName(name);
+        return this;
+    }
+
+    private void SetName(string name)
+    {
+        Name = Check.NotNullOrWhiteSpace(name, nameof(name), maxLength: AuthorConsts.MaxNameLength);
+    }
+}
